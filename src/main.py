553063cade05c8db76a5c8a924164a9b14b24728
MsZoezo <zoey@zoezo.dev>
2024-05-29 00:57:04 +0200

    Wowie the chat opens~

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 import sys
 import subprocess
 import time
+import webview
+import multiprocessing
 
-
+def open_chat(streamer):
+    webview.create_window('Chat', "https://www.twitch.tv/popout/{}/chat?popout=".format(streamer))
+    webview.start(private_mode=False)
 
 def main():
     if len(sys.argv) < 2:
@@ -13,12 +17,18 @@ def main():
     
     print("Loading mpv for twitch channel: {}".format(streamer))
 
-    stream = subprocess.Popen(["mpv", "https://www.twitch.tv/{}".format(streamer)], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
+    stream = subprocess.Popen(["mpv", "https://www.twitch.tv/{}".format(streamer)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+
+    chat_process = multiprocessing.Process(target=open_chat, args=(streamer,))
+    chat_process.start()
 
     while True:
         return_code = stream.poll()
         
         if return_code is not None:
+            # Gotta make sure we kill the chat aswell >.>
+            chat_process.kill()
+
             return return_code
         
         time.sleep(.3)
