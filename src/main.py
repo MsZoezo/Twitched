5696b0790fd8021e64d12d20d2d50a6d31c20129
MsZoezo <zoey@zoezo.dev>
2024-05-29 00:35:26 +0200

    Wow we open a stream teehee

--- src/main.py
+++ src/main.py
@@ -1,2 +1,24 @@
+import sys
+import subprocess
+import time
+
+
+
 def main():
-    print("Hello world!")
+    if len(sys.argv) < 2:
+        print("Missing argument for the twitch channel! (silly)")
+        return 1
+    
+    streamer = sys.argv[1]
+    
+    print("Loading mpv for twitch channel: {}".format(streamer))
+
+    stream = subprocess.Popen(["mpv", "https://www.twitch.tv/{}".format(streamer)], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
+
+    while True:
+        return_code = stream.poll()
+        
+        if return_code is not None:
+            return return_code
+        
+        time.sleep(.3)
